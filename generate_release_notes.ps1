80fd8fb7611187597c0d6e3887bcd99452e5086c
Eliazar Inso <75558018+Deadbush225@users.noreply.github.com>
2025-05-06 22:52:24 +0800

    version bump

---
+++ generate_release_notes.ps1
@@ -0,0 +1,47 @@
+# ━━━━━━━━━━━━━━━━━━━━ UPDATE repository FOLDER ━━━━━━━━━━━━━━━━━━━━ #
+# Run the update_repository.ps1 script
+# & "./scripts/update_repository.ps1"
+# ━━━━━━━━━━━━━━━━━━━━━━━ BUILD RELEASE NOTES ━━━━━━━━━━━━━━━━━━━━━━ #
+
+# Define the paths to the installer file and the markdown file
+# $installerPath = "./FolderCustomizerSetup-x64.exe"
+# $release_template = "./scripts/release_template.md"
+$release_notes = "./release_notes.md"
+
+# Read the version from a JSON file
+# $jsonFilePath = "./manifest.json"
+# $jsonContent = Get-Content -Path $jsonFilePath -Raw | ConvertFrom-Json
+# $version = $jsonContent.version
+
+# # Calculate the SHA256 hash of the installer file
+# $hash = Get-FileHash -Path $installerPath -Algorithm SHA256 | Select-Object -ExpandProperty Hash
+
+# # Read the content of the markdown file
+# $markdownContent = Get-Content -Path $release_template -Raw
+
+# $markdownContent = $markdownContent -replace "%HASH%", $hash
+
+# $markdownContent = $markdownContent -replace "%TITLE%", $version
+
+# # Write the updated content back to the markdown file
+# Set-Content -Path $release_notes -Value $markdownContent
+
+# Write-Host "Release notes for " $version "built"
+
+# ━━━━━━━━━━━━━━━━━━━━━━━━━ CREATE RELEASE ━━━━━━━━━━━━━━━━━━━━━━━━━ #
+$version = "v1.17.6"
+$arguments = @(
+    "release create",
+    $version,
+    "./manifest.json",
+    "./main.js", 
+    "./styles.css",
+    "--title",
+    "$version",
+    "--notes-file",
+    $release_notes,
+    "--repo", 
+    "Deadbush225/obsidian-projects"
+)
+Start-Process "gh" -ArgumentList ($arguments -join " ") -NoNewWindow -Wait
+Write-Host "Release created"
